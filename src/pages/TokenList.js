11989a93462cdc12abedd75d73a5d5f3c3d2b85d
Steven Yulong Yan <yulong.yan@uqconnect.edu.au>
2021-02-07 12:31:13 +1000

    Display owned lists

--- src/pages/TokenList.js
+++ src/pages/TokenList.js
@@ -1,24 +1,26 @@
 import {getAmount, round} from "../util/helpers";
+import "./TokenList.css";
 
 export const TokenList = (prop) => {
   return (
     <>
-      {
-        /*
-        This is just syntactic sugar for React.Fragment which essentially acts as a div
-        without the applied styles of a div
-        */
-      }
-      {prop.tokens.map(token =>
-        <div className="token" data-address={token.address} data-name={token.name}>
-          <div className="name item">
-            ${token.name} - ${token.symbol} - ${token.address}
-          </div>
-          <div className="value item">
-            Amount: ${round(getAmount(token), 2)}
-          </div>
-        </div>
-      )}
+      <div className="tokens">
+        {
+          prop.tokens.map(token =>
+            <div className="token" data-address={token.address} data-name={token.name}>
+              <div className="name item">
+                ${token.name}
+              </div>
+              <div className="value item">
+                Amount: ${round(getAmount(token), 2)}
+              </div>
+              <div className="address item">
+                ${token.address}
+              </div>
+            </div>
+          )
+        }
+      </div>
     </>
   )
 }
